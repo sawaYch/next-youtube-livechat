9c3c0b4a5bcb21b92efcf22cdd109931b91f259f
Sawa <34698182+sawaYch@users.noreply.github.com>
2024-03-27 15:35:54 +0800

    fix(ci): packages eslint itegration issue

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -5,7 +5,7 @@ const project = resolve(process.cwd(), "tsconfig.json");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
-    // "eslint:recommended",
+    "eslint:recommended",
     "prettier",
     "eslint-config-turbo",
     "next/core-web-vitals",
